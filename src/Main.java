9a89aa31e908573cb7e4ba53742525d6dfb843fa
Seandunn92 <seandunn92@gmail.com>
2017-04-11 14:21:50 -0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Created by seandunn92 on 4/11/17.
+ */
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args){
+        double width = -1.0;
+        double height = -1.0;
+        Scanner scnr= new Scanner(System.in);
+
+       System.out.println("Enter in the width");
+       width = scnr.nextDouble();
+       System.out.println("Enter in the length");
+
+
+    }
+}
